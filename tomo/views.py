41b2cdf3813eb3e79762fd5520b85b7b68ebf095
ThanhDang-player <s1f101910016@iniad.org>
2019-12-12 15:36:00 +0900

    Add Log In feature
    - Add path 'login'
    - Add views.login
    - Add login.html

--- tomo/views.py
+++ tomo/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            # [0] because it is a query set
+            user = User.objects.filter(username=username)[0]
+        except:
+            context = {
+                'wrongUsername': True 
+            }
+            return render(request, 'tomo/login.html', context)
+        if password != user.password:
+            context = {
+                'wrongPassword': True
+            }
+            return render(request, 'tomo/login.html', context)
+        else:
+            context = {}
+            print("OK")
+            return render(request, 'tomo/login.html', context)
+    else:
+        contex = {}
+        return render(request, 'tomo/login.html', context)
