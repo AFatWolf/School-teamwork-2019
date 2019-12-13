7ef4d5364f1c78b4d96f796ed4247fa280c72385
THANT MIN HTET <s1f101910050@iniad.org>
2019-12-13 13:43:09 +0900

    Update views.py

--- tomo/views.py
+++ tomo/views.py
@@ -36,10 +36,10 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            #return redirect('home')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            #login(request, user)
+            return redirect('templates\login.html')
     else:
         form = UserCreationForm()
         
