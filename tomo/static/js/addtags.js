20fafa73d512c4f10bad822fdca4a93b94a148d8
ThanhDang-player <s1f101910016@iniad.org>
2020-01-10 17:24:38 +0900

    Duc's comumunism

--- tomo/static/js/addtags.js
+++ tomo/static/js/addtags.js
@@ -13,21 +13,17 @@ $('document').ready(() => {
             let checkbox = document.getElementById(`tags_${id}`);
             if(checkbox.checked == false) {
                 checkbox.checked = true;
-                // let choosen = document.querySelector(`div.choosen[value='${id}']`);
-                // choosen.style.display = 'inline-block';
-                // let not_choosen = document.querySelector(`div.not_choosen[value='${id}']`);
-                // not_choosen.style.display = 'none';
-                let tags_box = document.querySelector(`div.tags_box[value='${id}']`);
-                tags_box.className = tags_box.className.replace("not_choosen", "choosen");
+                let choosen = document.querySelector(`div.choosen[value='${id}']`);
+                choosen.style.display = 'inline-block';
+                let not_choosen = document.querySelector(`div.not_choosen[value='${id}']`);
+                not_choosen.style.display = 'none';
             }
             else {
                 checkbox.checked = false;
-                // let choosen = document.querySelector(`div.choosen[value='${id}']`);
-                // choosen.style.display = 'none';
-                // let not_choosen = document.querySelector(`div.not_choosen[value='${id}']`);
-                // not_choosen.style.display = 'inline-block';
-                let tags_box = document.querySelector(`div.tags_box[value='${id}']`);
-                tags_box.className = tags_box.className.replace("choosen", "not_choosen");
+                let choosen = document.querySelector(`div.choosen[value='${id}']`);
+                choosen.style.display = 'none';
+                let not_choosen = document.querySelector(`div.not_choosen[value='${id}']`);
+                not_choosen.style.display = 'inline-block';
             }
         });
         tags_box[i].style.display = 'inline-block';
