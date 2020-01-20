6e2994c9ed3250d07812d8625de6e93f9e88cf6f
THANT MIN HTET <s1f101910050@iniad.org>
2020-01-20 13:46:23 +0900

    some css for settings

--- tomo/static/js/settings/pw_update.js
+++ tomo/static/js/settings/pw_update.js
@@ -1,6 +1,16 @@
-/* this is the password input field */
-var password = target.previousElementSibling;
-( 'password' == password.getAttribute('type') ) ? password.setAttribute('type', 'text') : 
-password.setAttribute('type', 'password');
-target.textContent = ( 'Hide' == target.textContent ) ? 'Show' : 'Hide';
-putCursorAtEnd(password);
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
